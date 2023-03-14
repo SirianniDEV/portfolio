28538d2ff72d5fdaa5c1bf8f5006c4ae5ed8149f
SofiaBCIT <sofia.sirianni@outlook.comm>
2023-03-13 23:29:06 -0700

    landing page test 1

---
+++ pages/gradient.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+        <div className={styles.gradientLanding}>
+            <div className={styles.gradientContainer}>
+            <h1>Sofia</h1>
+            <h2>Web Designer </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque ornare purus, consectetur finibus diam rutrum et. In aliquam libero sit amet odio finibus, sit amet feugiat lacus viverra</p>
+            </div>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+
+  )}
